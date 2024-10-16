7dd2dbd9fdc733a3ea0d07192325e0ce5215bfac
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-15 20:56:02 -0500

    feat(favorite-sources): add program configuration.

--- CatchUpPlatform.API/Program.cs
+++ CatchUpPlatform.API/Program.cs
@@ -1,13 +1,63 @@
+using CatchUpPlatform.API.News.Application.Internal.CommandServices;
+using CatchUpPlatform.API.News.Application.Internal.QueryServices;
+using CatchUpPlatform.API.News.Domain.Repositories;
+using CatchUpPlatform.API.News.Domain.Services;
+using CatchUpPlatform.API.News.Infrastructure.Persistence.EFC.Repositories;
+using CatchUpPlatform.API.Shared.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+// Add Database Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString is null)
+    throw new Exception("Database connection is not set.");
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
+    });
+else if (builder.Environment.IsProduction())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableDetailedErrors();
+    });
+
+// Configure Dependency Injection
+builder.Services.AddScoped<IUnitOfWOrk, UnitOfWork>();
+
+// News Bounded Context Dependency Injection
+builder.Services.AddScoped<IFavoriteSourceRepository, FavoriteSourceRepository>();
+builder.Services.AddScoped<IFavoriteSourceCommandService, FavoriteSourceCommandService>();
+builder.Services.AddScoped<IFavoriteSourceQueryService, FavoriteSourceQueryService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
