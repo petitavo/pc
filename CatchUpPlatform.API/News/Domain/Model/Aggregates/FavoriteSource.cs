d29b985089b04c818c48d69320939e3d18f45af4
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-19 11:56:39 -0500

    feat(favorite-sources): add audit trail to favorite source aggregate root.

--- CatchUpPlatform.API/News/Domain/Model/Aggregates/FavoriteSource.cs
+++ CatchUpPlatform.API/News/Domain/Model/Aggregates/FavoriteSource.cs
@@ -6,7 +6,7 @@ namespace CatchUpPlatform.API.News.Domain.Model.Aggregates;
 /// <summary>
 /// This class represents the FavoriteSource aggregate. It is used to store the favorite sources of a user.
 /// </summary>
-public class FavoriteSource
+public partial class FavoriteSource
 {
     public int Id { get; private set; }
     public string NewsApiKey { get; private set; }
