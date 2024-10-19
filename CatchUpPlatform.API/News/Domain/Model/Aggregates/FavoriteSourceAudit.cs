ccd01d806be51a7edae8b002d461a25ab68e2494
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-19 11:59:08 -0500

    docs: update field documentation comments.

--- CatchUpPlatform.API/News/Domain/Model/Aggregates/FavoriteSourceAudit.cs
+++ CatchUpPlatform.API/News/Domain/Model/Aggregates/FavoriteSourceAudit.cs
@@ -3,8 +3,10 @@ using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
 
 namespace CatchUpPlatform.API.News.Domain.Model.Aggregates;
 
+
 public partial class FavoriteSource : IEntityWithCreatedUpdatedDate
 {
+    // The CreatedDate and UpdatedDate properties are used to store the date and time when the entity was created and updated.
     [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
     [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
 }
